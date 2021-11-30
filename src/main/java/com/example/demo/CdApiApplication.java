2395dd3789f9f761ad96a0ec4540c03afba3143a
Dandingo <daniel.higgins7@btinternet.com>
2021-11-30 10:24:42 +0000

    first commit

---
+++ src/main/java/com/example/demo/CdApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdApiApplication.class, args);
+	}
+
+}
